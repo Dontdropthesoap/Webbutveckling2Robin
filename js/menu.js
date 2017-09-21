dbd2b10c8084f63da83f46ca2c46b58b38404ba4
Dontdropthesoap <31272725+Dontdropthesoap@users.noreply.github.com>
2017-09-21 12:31:54 +0200

    Javascript intro

---
+++ js/menu.js
@@ -0,0 +1,21 @@
+console.log("hej jag gillar lingondricka" +
+var text1 = "BLUB";
+var nummer1 = 23
+var decnumber = 32.34
+var boolean1 = true;
+var boolean = false;
+
+console .log(boolean1 + " " + nummer1 + decnumber); 
+console.log(nummer1 + decnumber);
+
+function blub() {
+	console.log("the blub funktion");
+	
+}
+function blub2(var parm) {
+	console.log(parm + text1);
+	
+}
+blub();
+
+
